e1f4e4bfb68016604d4c37eccd07964d6f0a7c4c
dronadal <enadal@nd.edu>
2021-10-18 16:18:53 -0500

    feat: added layout for header and about section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,92 @@
-import logo from "./logo.svg";
 import "./App.css";
+
+const backgroundUrl = "/assets/mainHeader.jpg";
+const aboutImageOne = "/assets/exampleImage1.jpg";
+const aboutImageTwo = "/assets/exampleImage2.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("${backgroundUrl}")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="text-center hero-content text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <section className="bg-gray-100 border-b py-8">
+        <div className="container max-w-5xl mx-auto m-8">
+          <h2 className="w-full my-2 text-5xl font-black leading-tight text-center text-gray-800">
+            Title
+          </h2>
+          <div className="w-full mb-4">
+            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          </div>
+
+          <div className="flex flex-wrap">
+            <div className="w-5/6 sm:w-1/2 p-6">
+              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                Lorem ipsum dolor sit amet
+              </h3>
+              <p className="text-gray-600 mb-8">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                at ipsum eu nunc commodo posuere et sit amet ligula.
+              </p>
+            </div>
+            <div className="w-full sm:w-1/2 p-6">
+              <img
+                className="mx-auto"
+                src={aboutImageOne}
+                alt="Example paintball"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-wrap flex-col-reverse sm:flex-row">
+            <div className="w-full sm:w-1/2 p-6 mt-6">
+              <img
+                className="mx-auto"
+                src={aboutImageTwo}
+                alt="Example paintball"
+              />
+            </div>
+            <div className="w-full sm:w-1/2 p-6 mt-6">
+              <div className="align-middle">
+                <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                  Lorem ipsum dolor sit amet
+                </h3>
+                <p className="text-gray-600 mb-8">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-base text-primary p-3">
+          Photos by{" "}
+          <a href="https://www.pexels.com/@bulat?utm_content=attributionCopyText&amp;utm_medium=referral&amp;utm_source=pexels">
+            Bulat Khamitov
+          </a>{" "}
+          from{" "}
+          <a href="https://www.pexels.com/photo/a-person-holding-a-paintball-gun-5619640/?utm_content=attributionCopyText&amp;utm_medium=referral&amp;utm_source=pexels">
+            Pexels
+          </a>
+        </div>
+      </section>
+    </>
   );
 }
 
