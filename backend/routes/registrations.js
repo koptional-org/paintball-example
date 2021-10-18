66c327f0205de160969ffa0cb1ca52148454fc57
consofine <michaelconsidine2020@u.northwestern.edu>
2021-10-18 16:58:35 -0500

    feat(registration): initial pass at db operations
    added ability to save data on POST and get rows on GET

--- backend/routes/registrations.js
+++ backend/routes/registrations.js
@@ -2,14 +2,32 @@ const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const states = require("../utils/states");
+const sqlite3 = require("sqlite3").verbose();
 
 /**
  * fetch schedule
  */
 router.get("/", function (req, res) {
   // Fetch from the database
+  const db = new sqlite3.Database("./paintball.db", (err) => {
+    if (err) {
+      console.log(err.message);
+    }
+    console.log("Connected to db");
+  });
 
-  res.send({ success: true });
+  db.all(
+    `SELECT phone, email, address, state, city, name FROM registrations;`,
+    [],
+    (err, rows) => {
+      if (err) {
+        throw new Error(err.message);
+      }
+      res.send({ rows: rows });
+      return rows;
+    }
+  );
+  db.close();
 });
 
 /**
@@ -28,9 +46,34 @@ router.post(
   (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      console.log("Got an error");
       return res.status(400).json({ errors: errors.array() });
     }
-    // Cerate a new record in db
+    const db = new sqlite3.Database("./paintball.db", (err) => {
+      if (err) {
+        console.log(err.message);
+      }
+    });
+
+    db.run(
+      `INSERT INTO registrations(phone, email, address, state, city, name) VALUES(?,?,?,?,?,?)`,
+      [
+        req.body["phone"],
+        req.body["email"],
+        req.body["address"],
+        req.body["state"],
+        req.body["city"],
+        req.body["name"],
+      ],
+      (err) => {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log(`A row has been inserted with row ID ${this.lastID}`);
+        db.close();
+      }
+    );
+    // Create a new record in db
     res.send({ success: true });
   }
 );
