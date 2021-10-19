d5887357d1469d1bdfe2731a1ac2ea046431865c
dronadal <enadal@nd.edu>
2021-10-19 13:43:24 -0500

    feat: formatted events being received to be compatible with fullcalendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "./App.css";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import BookSessionModal from "./BookSessionModal";
+import { format } from "date-fns";
 
 const backgroundUrl = "/assets/mainHeader.jpg";
 const aboutImageOne = "/assets/exampleImage1.jpg";
@@ -12,13 +13,26 @@ const aboutImageTwo = "/assets/exampleImage2.jpg";
 
 function App() {
   const [bookingTimeSelected, setBookingTimeSelected] = useState();
-  const [events, setEvents] = useState({});
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8000/registrations")
       .then((response) => response.json())
       .then((data) => setEvents(data.rows));
   }, []);
+
+  const formattedEvents = useMemo(() => {
+    let modifiedEvents = [];
+    (events || []).map((event) =>
+      modifiedEvents.push({
+        title: event.name,
+        start: format(event.date, "yyyy-MM-dd HH:mm"),
+        id: event.date,
+        backgroundColor: `${event.hasSignedWaiver === 0 ? "red" : "green"}`,
+      })
+    );
+    return modifiedEvents;
+  }, [events]);
 
   const addEvent = (values) => {
     const requestOptions = {
@@ -66,13 +80,17 @@ function App() {
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="text-center flex justify-center align-center text-neutral w-full container">
           <div className="md:w-8/12 lg:6/12 bg-white shadow-xl rounded p-3 h-auto">
+            <h3 className="text-2xl text-gray-800 font-bold leading-none mb-3">
+              Welcome! If your event is red please check your email to sign our
+              waiver.
+            </h3>
             <FullCalendar
               initialView="timeGridWeek"
               firstDay={1}
               slotMinTime="09:00:00"
               slotMaxTime="18:00:00"
-              slotLabelInterval="00:20:00"
-              slotDuration="00:20:00"
+              slotLabelInterval="00:30:00"
+              slotDuration="00:30:00"
               dateClick={(info) => {
                 var date = new Date(info.dateStr);
                 var today = new Date();
@@ -84,7 +102,7 @@ function App() {
               }}
               allDaySlot={false}
               plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-              events={events}
+              events={formattedEvents}
             />
           </div>
         </div>
