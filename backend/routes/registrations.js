608bf6ebc0275777bb1d478e2597be95954bc411
dronadal <enadal@nd.edu>
2021-10-19 09:30:19 -0500

    fix: added header to allow cors

--- backend/routes/registrations.js
+++ backend/routes/registrations.js
@@ -19,11 +19,6 @@ db.run(createTable, [], function (err) {
 });
 db.close();
 
-router.all("/*", function (req, res, next) {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
-  next();
-});
-
 /**
  * fetch schedule
  */
